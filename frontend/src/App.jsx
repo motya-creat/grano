7dfb2c9083ead806c742f00fe3e157de993430cb
Your Name <you@example.com>
2026-01-26 21:04:48 +0300

    rollback

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,13 @@
 import './App.css';
 import Header from './assets/components/header/header.jsx';
-import RegForm from './assets/components/form.jsx';
+
 function App() {
 
   return (
     <>
       <Header/>
-      <RegForm></RegForm>
     </>
   )
 }
 
 export default App
-
