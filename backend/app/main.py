d2343d00f4283455d063c03f08bfe59acf91ed92
Your Name <you@example.com>
2026-01-26 21:20:34 +0300

    adding the first backend and frontend connection

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,36 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI
+app = FastAPI()
+
+# --- ВАЖНО: Настройка CORS ---
+# Чтобы React (на порту 3000 или 5173) мог достучаться до FastAPI (порт 8000)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # В разработке разрешаем всем, в деплое укажешь домен сайта
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Описываем структуру данных, которые ждем от фронтенда
+class FormData(BaseModel):
+    text: str
+
+@app.post("/send-data")
+async def receive_data(data: FormData):
+    print(f"Получены данные: {data.text}")
+    # Возвращаем ответ фронтенду
+    return {"status": "success", "received": data.text}
 
 
 
-@app.get("/")
-def main():
-    return "Hello World"
 
 
+
+
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css';
 import Header from './assets/components/header/header.jsx';
-
+import RegForm from './assets/components/form.jsx';
 function App() {
 
   return (
     <>
       <Header/>
+      <RegForm></RegForm>
     </>
   )
 }
 
 export default App
+
