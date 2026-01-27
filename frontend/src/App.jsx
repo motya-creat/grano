ddb40b43596547aed9b4a781fdad370cc393b627
Your Name <you@example.com>
2026-01-27 12:48:15 +0300

    add footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './assets/components/header/header.jsx';
+import Footer from './assets/components/footer/footer.jsx';
 
 function App() {
 
   return (
     <>
       <Header/>
+      <Footer/>
     </>
   )
 }
